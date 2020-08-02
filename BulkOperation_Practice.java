8ae4bcb3b165e5b51a0088f3d3f012831a80014d
sevdaTan <sevdatanyildizi@gmail.com>
2020-08-02 12:50:09 -0400

    update practice

--- BulkOperation_Practice.java
+++ BulkOperation_Practice.java
@@ -28,5 +28,25 @@ public class BulkOperation_Practice {
         boolean result = group14.containsAll(Arrays.asList("Muhtar","Asiya","Kuzzat"));
         System.out.println(result);
 
+        System.out.println("======================= retainAll =================");
+
+        ArrayList<String> employees =new ArrayList<>();
+        employees.addAll( Arrays.asList( "Ahmed", "Muhtar", "Ahmed", "Virginia", "Beslan", "Ibrahim", "Ahmed") );
+
+        System.out.println(employees);
+
+        employees.retainAll(  Arrays.asList("Ahmed", "Dawud")  );
+        System.out.println(employees);
+
+        System.out.println("====================== removeAll ==================");
+
+        ArrayList<String> employees2 =new ArrayList<>();
+        employees2.addAll( Arrays.asList( "Ahmed", "Muhtar", "Ahmed", "Virginia", "Beslan", "Ibrahim", "Ahmed") );
+
+        System.out.println(employees2);
+
+        employees2.removeAll(  Arrays.asList("Ahmed")  );
+        System.out.println(employees2);
+
     }
 }
